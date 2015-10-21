2a444177bcd026a730db44c5605dac21b5cf1919
Robin Norwood <robin.norwood@rackspace.com>
2015-10-21 13:32:23 -0500

    Fix right-turning bug.

--- asteroids4.py
+++ asteroids4.py
@@ -199,7 +199,7 @@ class Player(Entity):
             degrees = (degrees + 10) % 360
 
         if self.turn_right:
-            degrees = (degrees + 10) % 360
+            degrees = (degrees - 10) % 360
 
         self.facing = Vector.from_degrees(degrees)
 
